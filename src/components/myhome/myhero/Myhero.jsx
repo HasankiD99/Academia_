94a58962232789a9014ba0e78b03bcc75d3cde82
Hasanki Dharmarathna <didulanidharmarathna@gmail.com>
2024-12-07 16:45:57 +0530

    add image

--- src/components/myhome/myhero/Myhero.jsx
+++ src/components/myhome/myhero/Myhero.jsx
@@ -3,10 +3,25 @@ import "./myhero.css"
 
 function Myhero() {
   return (
-    <div>
-      <h1>Hero</h1>
-    </div>
-  )
+    < >
+      <section>
+        <div className="container">
+          <div className="row">
+            <p>bhj bjhb bjkbkj bjkbk</p>
+            <div className="button">
+              <button classname="primary-btn">
+                GET STARTED NOW<i className="fa fa-long-arrow-alt-right"></i>
+              </button>
+              <button className="primary-btn">
+                VIEW COURSE<i className="fa fa-long-arrow-alt-right"></i>
+              </button>
+            </div>
+          </div>
+        </div>
+      </section>
+ <div className="margin"></div>
+    </ >
+  );
 }
 
 export default Myhero
