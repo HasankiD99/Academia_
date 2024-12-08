a4f626a07339166d8341b99a5ed3796cfa5c95b3
Hasanki Dharmarathna <didulanidharmarathna@gmail.com>
2024-12-08 10:19:12 +0530

    add some files

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import Myhome from './components/myhome/Myhome';
+import About from './components/about/About';
 
 const App = () => {
   return (
@@ -13,7 +14,7 @@ const App = () => {
         <Header />
         <Switch>
           <Route path='/' exact component={Myhome} />
-            
+          <Route path='/about' exact component={About} /> 
         </Switch>
       </Router>
     </div>
--- src/components/myhome/myhero/Myhero.jsx
+++ src/components/myhome/myhero/Myhero.jsx
@@ -1,27 +1,35 @@
-import React from 'react'
-import "./myhero.css"
+import React from "react";
+import "./myhero.css";
+import Title from "../../common/title/Title";
 
 function Myhero() {
   return (
-    < >
-      <section>
+    <>
+      <section className="hero">
         <div className="container">
           <div className="row">
-            <p>bhj bjhb bjkbkj bjkbk</p>
+            <Title subtitle={'WELCOME TO ACEDEMIA'} title={'Best Online Education'} />
+            <p>
+              Unlock your potential with our comprehensive courses and
+              expert-led programs. Whether you’re looking to enhance your
+              skills, advance your career, or explore new passions, ACADEMIA
+              provides top-quality education from the comfort of your home.
+            </p>
             <div className="button">
-              <button classname="primary-btn">
+              <button className="primary-btn">
                 GET STARTED NOW<i className="fa fa-long-arrow-alt-right"></i>
               </button>
-              <button className="primary-btn">
+              <button  >
                 VIEW COURSE<i className="fa fa-long-arrow-alt-right"></i>
               </button>
+               
             </div>
           </div>
         </div>
       </section>
- <div className="margin"></div>
-    </ >
+      <div className="margin"></div>
+    </>
   );
 }
 
-export default Myhero
+export default Myhero;
